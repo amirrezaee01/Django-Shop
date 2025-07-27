01dde951e85110ba6252ed925cff86c005c7c1fc
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-07-27 12:58:32 +0330

    Add logout functionality and update user avatar
    Introduced LogoutView and logout URL for user sign-out. Updated settings to include LOGOUT_REDIRECT_URL. Changed user avatar in base.html to use a static image and updated login/logout links to use named URLs. Added naruto.png to static images.

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -4,6 +4,7 @@ from .views import LoginView, LogoutView
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login', LoginView.as_view(), name='login'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
 ]
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import views as auth_views
 from accounts.forms import AuthenticationForm
-from django.contrib.auth.views import LogoutView
 
 
 class LoginView(auth_views.LoginView):
     template_name = 'accounts/login.html'
     redirect_authenticated_user = True
     form_class = AuthenticationForm
+    
+class LogoutView(auth_views.LogoutView):
+    pass
