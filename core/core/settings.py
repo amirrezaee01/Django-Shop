d6783b24706af28f473c53573b6e0ea7d4f7a51f
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-07-28 11:20:44 +0330

    create shop and its templaate

--- core/core/settings.py
+++ core/core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'website',
     'accounts',
+    'shop',
 ]
 
 MIDDLEWARE = [
---
+++ core/shop/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+app_name = 'shop'
+
+urlpatterns = [
+    path('product/grid/', ShopProductGridView.as_view(), name='product-grid'),
+]
