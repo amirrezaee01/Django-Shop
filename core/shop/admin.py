f686e40dbf7199d66a08ebb68f7bcd2781a487d1
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-07-29 12:12:51 +0330

    make discounted and paginate

--- core/shop/admin.py
+++ core/shop/admin.py
@@ -2,14 +2,18 @@ from django.contrib import admin
 from .models import ProductModel, ProductCategoryModel, ProductImageModel
 # Register your models here.
 
+
 @admin.register(ProductModel)
 class ProductModelAdmin(admin.ModelAdmin):
-    list_display = ('id','user', 'title', 'stock', 'status', 'created_date')
+    list_display = ('id', 'user', 'title', 'price', 'discount_percent',
+                    'stock', 'status', 'created_date')
+
 
 @admin.register(ProductCategoryModel)
 class ProductCategoryModelAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'created_date')
-    
+
+
 @admin.register(ProductImageModel)
 class ProductImageModelAdmin(admin.ModelAdmin):
-    list_display = ('id','file', 'created_date')
+    list_display = ('id', 'file', 'created_date')
--- core/shop/models.py
+++ core/shop/models.py
@@ -1,46 +1,62 @@
 from django.db import models
+from django.utils.functional import cached_property
 
 
 class ProductStatusType(models.IntegerChoices):
     publish = 1, ("نمایش")
     draft = 2, ("عدم نمایش")
 
+
 class ProductCategoryModel(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
-    
+
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return self.title
 
 
 class ProductModel(models.Model):
-    user = models.ForeignKey("accounts.User",on_delete=models.PROTECT)
+    user = models.ForeignKey("accounts.User", on_delete=models.PROTECT)
     category = models.ManyToManyField(ProductCategoryModel)
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
-    image = models.ImageField(default='/default/product-image.png', upload_to="product/img/")
+    image = models.ImageField(
+        default='/default/product-image.png', upload_to="product/img/")
     description = models.TextField()
     stock = models.IntegerField(default=0)
-    price = models.DecimalField(default=0,max_digits=10, decimal_places=0)
+    price = models.DecimalField(default=0, max_digits=10, decimal_places=0)
     discount_percent = models.IntegerField(default=0)
-    status = models.IntegerField(choices=ProductStatusType.choices, default=ProductStatusType.draft.value)
+    status = models.IntegerField(
+        choices=ProductStatusType.choices, default=ProductStatusType.draft.value)
 
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-    
-    class Mete:
+
+    class Meta:
+
         ordering = ['-created_date']
-        
+
     def __str__(self):
         return self.title
 
-    
+    @property
+    def is_discounted(self):
+        return self.discount_percent > 0
+
+    @property
+    def get_price(self):
+        if self.is_discounted:
+            discount = self.price * self.discount_percent / 100
+            return round(self.price - discount)
+        return round(self.price)
+
+
 class ProductImageModel(models.Model):
     product = models.ForeignKey(ProductModel, on_delete=models.CASCADE)
     file = models.ImageField(upload_to="product/extra-img/")
-    
+
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
