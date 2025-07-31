a3329dfa35f921bcfac37fda8a6ffd264814ad95
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-07-31 09:39:51 +0330

    fix the seatch bar and category

--- core/shop/views.py
+++ core/shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import ProductModel, ProductStatusType
+from .models import ProductModel, ProductStatusType, ProductCategoryModel
 from django.views.generic import (
     TemplateView,
     ListView,
@@ -10,13 +10,22 @@ from django.views.generic import (
 
 class ShopProductGridView(ListView):
     template_name = 'shop/product_grid.html'
-    queryset = ProductModel.objects.filter(
-        status=ProductStatusType.publish.value)
     paginate_by = 9
+
+    def get_queryset(self):
+        queryset = ProductModel.objects.filter(
+            status=ProductStatusType.publish.value)
+        if search_q := self.request.GET.get('q'):
+            queryset = queryset.filter(title__icontains=search_q)
+        if category_id := self.request.GET.get('category_id'):
+            queryset = queryset.filter(category__id=category_id)
+        return queryset  # <-- Add this line
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['total_items'] = self.queryset.count()
+        context['total_items'] = self.get_queryset().count()
+        context['categories'] = ProductCategoryModel.objects.filter(
+            productmodel__status=ProductStatusType.publish.value).distinct()
         return context
 
 
