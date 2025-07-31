e5dd31a1ad82cdcce18ab2cda2101cde82b3ab40
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-07-31 11:21:29 +0330

    complete the search bar

--- core/shop/views.py
+++ core/shop/views.py
@@ -5,12 +5,16 @@ from django.views.generic import (
     ListView,
     DetailView,
 )
+from django.core.exceptions import FieldError
 # Create your views here.
 
 
 class ShopProductGridView(ListView):
     template_name = 'shop/product_grid.html'
     paginate_by = 9
+
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get('page_size', self.paginate_by)
 
     def get_queryset(self):
         queryset = ProductModel.objects.filter(
@@ -19,7 +23,16 @@ class ShopProductGridView(ListView):
             queryset = queryset.filter(title__icontains=search_q)
         if category_id := self.request.GET.get('category_id'):
             queryset = queryset.filter(category__id=category_id)
-        return queryset  # <-- Add this line
+        if min_price := self.request.GET.get('min_price'):
+            queryset = queryset.filter(price__gte=min_price)
+        if max_price := self.request.GET.get('max_price'):
+            queryset = queryset.filter(price__lte=max_price)
+        if order_by := self.request.GET.get('order_by'):
+            try:
+                queryset = queryset.order_by(order_by)
+            except FieldError:
+                pass
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
