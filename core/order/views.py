7aaedcac9595ef69a6c4d6efd6f3db5876fed41b
amirrezaee01 <amirrezayi0101@gmail.com>
2025-10-05 11:13:49 +0330

    Add order completion view and template
    Introduces OrderCompletedView and its URL, along with a new completed.html template for order confirmation. Enhances checkout form validation to ensure address ownership and updates OrderCheckoutView to redirect to the completion page after successful checkout.

--- core/order/views.py
+++ core/order/views.py
@@ -1,16 +1,35 @@
 from django.views.generic import TemplateView,FormView
 from django.shortcuts import render
-from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from order.permissions import HasCustomerAccessPermission
 from order.models import UserAddressModel
 from cart.models import CartModel
 from order.forms import CheckOutForm
+from django.urls import reverse_lazy
 
 
 class OrderCheckoutView(LoginRequiredMixin, HasCustomerAccessPermission, FormView):
     template_name = 'order/checkout.html'
     form_class = CheckOutForm
+    success_url = reverse_lazy('order:completed')
+    
+    def get_form_kwargs(self):
+        kwargs = super(OrderCheckoutView, self).get_form_kwargs()
+        kwargs['request'] = self.request
+        return kwargs
+
+    def form_valid(self, form):
+        cleaned_data = form.cleaned_data
+        address = cleaned_data['address_id']
+        print(address)
+        return super().form_valid(form)
+
+        
+        
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+        
+    
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -20,3 +39,6 @@ class OrderCheckoutView(LoginRequiredMixin, HasCustomerAccessPermission, FormVie
         context['total_price'] = total_price
         context['total_tax'] = round((total_price * 9)/100)  
         return context
+    
+class OrderCompletedView(LoginRequiredMixin, HasCustomerAccessPermission, TemplateView):
+    template_name = 'order/completed.html'
